24a1fffd14512c63f5ff0ef56511fa4b216c651f
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-08-30 19:52:44 +0530

    add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
+  // entry: {
+  // index: "./src/index.js",
+  // },
+  entry: "./src/index.js",
   devtool: "inline-source-map", // figures out root file of the error
   devServer: {
     static: "./dist", //rebuilds when anything changes
@@ -30,6 +31,17 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
